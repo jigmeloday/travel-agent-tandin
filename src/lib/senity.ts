1014ae197bda1e81a266291f223c28d8bde00644
Jigme Lodey <jigmenlodey@gmail.com>
2026-01-15 10:33:15 +0600

    fix:

--- src/lib/senity.ts
+++ src/lib/senity.ts
@@ -4,5 +4,5 @@ export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,  // from sanity.json / manage.sanity.io
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,       // usually "production"
   apiVersion: "2024-01-01",                               // any recent date
-  useCdn: true,                                            // faster reads
+  useCdn: false,                                            // faster reads
 })
