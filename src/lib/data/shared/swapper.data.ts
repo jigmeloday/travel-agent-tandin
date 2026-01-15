95a391379705fedb6f494d57574a18d8754728f0
Jigme Lodey <jigmenlodey@gmail.com>
2026-01-15 12:25:18 +0600

    fix: form binding

--- src/lib/data/shared/swapper.data.ts
+++
@@ -1,7 +0,0 @@
-export const SWAPPER_DATA = [
-  { img: 'images/dummy/img7.jpg', id: 1, title: 'Discover Bhutan, Beyond Imagination', page: 'home' },
-  { img: 'images/dummy/img5.jpg', id: 2, title: 'Every Journey, Exclusively Yours', page: 'home' },
-  { img: 'images/slide.jpg', id: 3, title: 'Where Culture, Nature, and Luxury Meet', page: 'home' },
-  { img: 'images/dummy/img1.jpg', id: 4, title: 'Travel Deeper. Experience Bhutan', page: 'home' },
-  { img: 'images/dummy/img2.jpg', id: 5, title: 'Crafting Moments, Curating Memories', page: 'home' },
-]
